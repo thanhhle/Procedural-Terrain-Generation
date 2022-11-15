bacd00086201f6eb6adb447f9b86f745afca4c9c
thanhhle <thanhle.thle@gmail.com>
2022-11-14 18:55:07 -0800

    Generated 2-D color map

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -4,51 +4,76 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    public int Width;
-    public int Height;
-
-    public int Seed;
-    public float Scale;
-
-    public int Octaves;  
-    [Range(0, 1)]
-    public float Persistence;
-    public float Lacunarity;
-
-    public Vector2 Offset;
-
     public bool AutoUpdate;
+
+    public enum MapType {NoiseMap, ColorMap};
+    public MapType mapType;
+
+    public int width;
+    public int height;
+
+    public int seed;
+    public float scale;
+
+    public int octaves;  
+    [Range(0, 1)]
+    public float persistence;
+    public float lacunarity;
+
+    public Vector2 offset;
+
+    public Landform[] landforms;
 
 
     public void GenerateMap()
     {
-        float[,] noiseMap = NoiseMapGenerator.GenerateNoiseMap(Width, Height, Seed, Scale, Octaves, Persistence, Lacunarity, Offset);
+        float[,] noiseMap = NoiseMapGenerator.GenerateNoiseMap(width, height, seed, scale, octaves, persistence, lacunarity, offset);
+
+        Color[] colorMap = new Color[width * height];
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                float noiseValue = noiseMap[x, y];
+                for (int i = 0; i < landforms.Length; i++)
+                {
+                    if (noiseValue <= landforms[i].threshold)
+                    {
+                        colorMap[y * width + x] = landforms[i].color;
+                        break;
+                    }
+                }
+            }
+        }
 
         MapRenderer mapRenderer = FindObjectOfType<MapRenderer>();
-        mapRenderer.RenderMap(noiseMap);
-    }
-
-
-    private void Start() 
-    {
-        GenerateMap();
+        if (mapType == MapType.NoiseMap)
+        {
+            mapRenderer.RenderMap(TextureGenerator.GenerateTextureFromNoiseMap(noiseMap));
+        }
+        else if (mapType == MapType.ColorMap)
+        {
+            mapRenderer.RenderMap(TextureGenerator.GenerateTextureFromColorMap(colorMap, width, height));
+        }     
     }
 
 
     private void OnValidate() 
     {
-        Width = Width < 1 ? 1 : Width;
-        Height = Height < 1 ? 1 : Height;
-        Lacunarity = Lacunarity < 1 ? 1 : Lacunarity;
-        Octaves = Octaves < 0 ? 0 : Octaves;
+        width = width < 1 ? 1 : width;
+        height = height < 1 ? 1 : height;
+        scale = scale <= 0 ? 0.1f : scale;
+        lacunarity = lacunarity < 1 ? 1 : lacunarity;
+        octaves = octaves < 0 ? 0 : octaves;
     }
    
 }
 
 
-public struct TerrainType
+[System.Serializable]
+public struct Landform
 {
     public string name;
-    public float height;
+    public float threshold;
     public Color color;
 }
--- Assets/Scripts/MapRenderer.cs
+++ Assets/Scripts/MapRenderer.cs
@@ -4,27 +4,11 @@ using UnityEngine;
 
 public class MapRenderer : MonoBehaviour
 {
-    public Renderer TextureRenderer;
+    public Renderer renderer;
 
-    public void RenderMap(float[,] noiseMap)
+    public void RenderMap(Texture2D texture)
     {
-        int width = noiseMap.GetLength(0);
-        int height = noiseMap.GetLength(1);
-
-        Texture2D textureMap = new Texture2D(width, height);
-
-        for (int x = 0; x < width; x++)
-        {
-            for (int y = 0; y < height; y++)
-            {
-                Color color = Color.Lerp(Color.black, Color.white, noiseMap[x, y]);
-                textureMap.SetPixel(x, y, color);
-            }
-        }
-        
-        textureMap.Apply();
-
-        TextureRenderer.sharedMaterial.mainTexture = textureMap;
-        TextureRenderer.transform.localScale = new Vector3(width, 1, height);
+        renderer.sharedMaterial.mainTexture = texture;
+        renderer.transform.localScale = new Vector3(texture.width, 1, texture.height);
     }
 }
